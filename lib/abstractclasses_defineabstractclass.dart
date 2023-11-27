491320b8d92a1903a403a1330bb986615dfd1731
eshad <fidaeshad10@gmail.com>
2023-11-27 05:10:46 -0600

    added second question and test cases

--- lib/abstractclasses_defineabstractclass.dart
+++ lib/abstractclasses_defineabstractclass.dart
@@ -17,14 +17,54 @@ The class should have two abstract methods: writeData(String data) and readData(
 Implement print statements inside these methods (even though they are abstract).
  */
 
+/*
+Practice Question 2: Implement Concrete Classes
+
+Question:
+
+Extend the DataSource class with two concrete classes named FileStorage and MemoryStorage.
+
+Implement the writeData(String data) and readData() methods in these classes.
+ */
+
 abstract class DataSource {
-  void writeData(String data){
+  void writeData(String data) {
     print("This will write some data: $data");
   }
 
-  void readData(){
+  void readData() {
     print("This will read data");
   }
 
-  void needsToBeImplemented();
+  //void needsToBeImplemented();
+}
+
+class FileStorage extends DataSource {
+  String? data;
+
+  @override
+  void writeData(String data) {
+    print("Data written to file: $data");
+    this.data = data;
+  }
+
+  @override
+  void readData() {
+    print("Data written to file: $data");
+  }
+}
+
+class MemoryStorage extends DataSource {
+  String? data;
+  
+  @override
+  void writeData(String data) {
+    print("Data stored in memory: $data");
+    this.data = data;
+  }
+
+  @override
+  void readData() {
+    print("Data stored in memory: $data");
+  }
 }
--- test/abstractclasses_defineabstractclass_test.dart
+++ test/abstractclasses_defineabstractclass_test.dart
@@ -1,5 +1,33 @@
 import 'package:abstractclasses_defineabstractclass/abstractclasses_defineabstractclass.dart';
 import 'package:test/test.dart';
 
+void main() {
+  group('Testing Concrete Classes FileStorage and MemoryStorage:', () {
+
+    test('FileStorage should implement DataSource', () {
+      expect(FileStorage(), isA<DataSource>());
+    });
+
+    test('MemoryStorage should implement DataSource', () {
+      expect(MemoryStorage(), isA<DataSource>());
+    });
+
+    test('FileStorage should correctly write and read data', () {
+      var testData = 'file test data';
+      final x = FileStorage();
+      x.writeData(testData);
+      expect(
+          () => x.readData(), prints('Data written to file: $testData\n'));
+    });
+
+    test('MemoryStorage should correctly write and read data', () {
+      var testData = 'memory test data';
+      final y = MemoryStorage();
+      y.writeData(testData);
+      expect(
+          () => y.readData(), prints('Data stored in memory: $testData\n'));
+    });
+  });
+}
 
 
